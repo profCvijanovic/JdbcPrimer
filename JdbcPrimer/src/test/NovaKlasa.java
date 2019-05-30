2550972cd17592d1a2da9481f180cd35232eed8e
nesa <cvijanovic.n@oilpc.ru>
2019-05-30 15:58:43 +0200

    dodao sam i ja nesto

--- JdbcPrimer/src/test/NovaKlasa.java
+++ JdbcPrimer/src/test/NovaKlasa.java
@@ -3,6 +3,15 @@ package test;
 public class NovaKlasa {
 	
 	private String proba;
+	private int prviNestoDodaje;
+	
+	public int getPrviNestoDodaje() {
+		return prviNestoDodaje;
+	}
+
+	public void setPrviNestoDodaje(int prviNestoDodaje) {
+		this.prviNestoDodaje = prviNestoDodaje;
+	}
 
 	public String getProba() {
 		return proba;
