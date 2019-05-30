53b1fc21c78a217c4e8cac25b505f2ab13d9fb17
nesa <cvijanovic.n@oilpc.ru>
2019-05-30 15:39:17 +0200

    dodata nova klasa pred merge

--- JdbcPrimer/src/test/GlavnaKlasa.java
+++ JdbcPrimer/src/test/GlavnaKlasa.java
@@ -5,6 +5,7 @@ public class GlavnaKlasa {
 	public static void main(String[] args) {
 		
 		System.out.println("Branch novi!");
+		System.out.println("Ovo hocu da merdzujem!");
 
 	}
 
---
+++ JdbcPrimer/src/test/NovaKlasa.java
@@ -0,0 +1,17 @@
+package test;
+
+public class NovaKlasa {
+	
+	private String proba;
+
+	public String getProba() {
+		return proba;
+	}
+
+	public void setProba(String proba) {
+		this.proba = proba;
+	}
+	
+	
+
+}
