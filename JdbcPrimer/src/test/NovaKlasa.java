60a4ddbc29200b65447f30c326943f437bf15b31
nesa <cvijanovic.n@oilpc.ru>
2019-05-30 15:46:52 +0200

    Drugi branch aktiviran

--- JdbcPrimer/src/test/NovaKlasa.java
+++ JdbcPrimer/src/test/NovaKlasa.java
@@ -3,6 +3,17 @@ package test;
 public class NovaKlasa {
 	
 	private String proba;
+	private int dodatoUDrugomBranchu;
+	
+	
+
+	public int getDodatoUDrugomBranchu() {
+		return dodatoUDrugomBranchu;
+	}
+
+	public void setDodatoUDrugomBranchu(int dodatoUDrugomBranchu) {
+		this.dodatoUDrugomBranchu = dodatoUDrugomBranchu;
+	}
 
 	public String getProba() {
 		return proba;
