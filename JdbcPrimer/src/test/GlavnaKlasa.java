062bbeafe4b66a9de959ad92117132255e82e357
nesa <cvijanovic.n@oilpc.ru>
2019-05-30 15:36:37 +0200

    Glavna bez mergea

--- JdbcPrimer/src/test/GlavnaKlasa.java
+++ JdbcPrimer/src/test/GlavnaKlasa.java
@@ -3,7 +3,8 @@ package test;
 public class GlavnaKlasa {
 
 	public static void main(String[] args) {
-		System.out.println("Poz goco");
+		System.out.println("Glavna klasa");
+		System.out.println("Nije merdzovano jos...");
 
 	}
 
